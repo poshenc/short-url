b369b944f44a4ea4391399f20f7bcf031b4aea05
johnson <poshenc@gmail.com>
2021-07-29 23:33:43 +0800

    styling javascript and minor modifications

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,6 @@ const codeGenerator = require('../../tools/codeGenerator')
 const hostUrl = process.env.mainUrl || 'http://localhost:3000/'
 
 router.get('/', (req, res) => {
-  console.log(hostUrl)
   res.render('index')
 })
 
